c1ab429d3ccbf51a80766fdbf757315c01eacdd9
1103-13 <1103-13@1103-13-PC>
2018-03-09 17:46:26 +0900

    회원가입진행

--- src/total/controller/NavController.java
+++ src/total/controller/NavController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import total.service.GreetService;
 
@@ -14,7 +15,7 @@ public class NavController {
 	@Autowired
 	GreetService greetService;
 	
-	@RequestMapping("/join")
+	@RequestMapping(path="/join", method=RequestMethod.GET)
 	public String joinHandle(Model model) {
 		model.addAttribute("ment", greetService.make());
 		return "join";
